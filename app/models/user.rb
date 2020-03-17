d27342d818a4e3ee1c58ad6b397432e1af2c3b77
TashfeenRao <raotashfeen25@gmail.com>
2020-03-17 21:13:53 +0500

    omni auth is added

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, :omniauth_providers => [:facebook]
+         :omniauthable, omniauth_providers: [:facebook]
 
   validates :name, presence: true, length: { maximum: 20 }
 
@@ -15,8 +15,8 @@ class User < ApplicationRecord
 
   def self.new_with_session(params, session)
     super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-      user.email = data["email"] if user.email.blank?
+      if data == session['devise.facebook_data'] && session['devise.facebook_data']['extra']['raw_info']
+        user.email = data['email'] if user.email.blank?
       end
     end
   end
@@ -24,22 +24,22 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
+      user.password = Devise.friendly_token[0, 20]
       user.name = auth.info.name # assuming the user model has a name
       user.gravatar_url = auth.info.image # assuming the user model has an image
     end
   end
 
   def show_requests
-    current_user.inverse_friendships.where(status: false)
+    @request = current_user.inverse_friendships.where(status: false)
   end
 
   def show_pending
-    current_user.friendships.where(status: false)
+    @pending = current_user.friendships.where(status: false)
   end
 
   def friends
-    current_user.friendships.where(status: true)
+    @friend = current_user.friendships.where(status: true)
   end
 
   def user_friends(user)
@@ -53,7 +53,4 @@ class User < ApplicationRecord
 
     @mutual = u1 if u1.friend_id == u2.friend_id
   end
-
 end
-
-
